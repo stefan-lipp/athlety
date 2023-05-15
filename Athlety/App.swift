bcca152ed8558ee5f3c98db1b39f33975d1fecb9
Stefan Cimander <stefan.cimander@me.com>
2023-05-15 05:31:57 +0200

    Add initial iOS Xcode project

---
+++ Athlety/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  Athlety
+//
+//  Created by Stefan Cimander on 15.05.23.
+//
+
+import SwiftUI
+
+@main
+struct AthletyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
